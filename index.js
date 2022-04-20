293441642b7aca4c0264f5cc7921f6484e7fc42b
Kevin Kelley <kkelley622@gmail.com>
2022-04-20 10:38:55 -0500

    commit tree 7025c8fb311cf42cece1f6b03a09b977dec9349a

--- index.js
+++ index.js
@@ -1,5 +1,90 @@
 /* Your Code Here */
 
+// Loads array elements into corresponding object properties. Additionally, initialize empty arrays on the properties
+// timeInEvents and timeOutEvents
+
+function createEmployeeRecord(recArray) {
+    return {
+        firstName: recArray[0],
+        familyName: recArray[1],
+        title: recArray[2],
+        payPerHour: recArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+// Converts each nested array into an employee record using createEmployeeRecord and accumulates it to a new array
+
+function createEmployeeRecords(recordsArray) {
+    return recordsArray.map(rec => createEmployeeRecord(rec))
+}
+
+// Add an object with keys:
+// type: set to "TimeIn"
+// hour: derived from argument
+// date: derived from argument
+
+function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ")
+    const inEvent = {
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date
+    }
+    this.timeInEvents.push(inEvent)
+    return this 
+
+}
+
+// Add an Object with keys:
+// type: "TimeOut"
+// hour: derived from the argument
+// date: derived from the argument
+
+function createTimeOutEvent(dateStamp) {
+    const[date, hour] = dateStamp.split(" ")
+    const outEvent = {
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date
+    }
+    this.timeOutEvents.push(outEvent)
+    return this 
+}
+
+// Given a date, find the number of hours elapsed between that date's timeInEvent
+// and timeOutEvent
+
+function hoursWorkedOnDate(targetDate) {
+    const hoursIn = this.timeInEvents.find(inEvent => inEvent.date === targetDate)
+    const hoursOut = this.timeOutEvents.find(outEvent => outEvent.date === targetDate)
+    return (hoursOut.hour - hoursIn.hour) / 100
+}
+
+// using hoursWorkedOnDate, multiply the hours by the record's payRate to 
+// determine amount owed. Amount should be returned as a number
+
+function wagesEarnedOnDate(targetDate) {
+    const hours = hoursWorkedOnDate.call(this, targetDate)
+    return this.payPerHour * hours
+}
+
+// Test the firstName field for a match with the firstName argument
+
+function findEmployeeByFirstName(employees, firstNameString) {
+    return employees.find(emp => emp.firstName === firstNameString)
+}
+
+// Using `allWagesFor` for each of the employees, accumulate the value of
+// all dates worked by the employee in the record used as context. Amount
+// should be returned as a number.
+
+function calculatePayroll(recordsArray) {
+    const record = recordsArray.map(employee => allWagesFor.call(employee))
+    return record.reduce((currentValue, total) => currentValue + total)
+    
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
